4304b0d28634e6ec49af839476c08432ccc56352
HanSeoungJun <67749318+HanSeongJun@users.noreply.github.com>
2022-01-20 06:40:30 +0900

    ADD : addForm
    상품 등록 폼 추가

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -35,6 +36,16 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "/basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "/basic/addForm";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
@@ -43,6 +54,4 @@ public class BasicItemController {
         itemRepository.save(new Item("testA", 10000, 10));
         itemRepository.save(new Item("testB", 20000, 20));
     }
-
-
 }
